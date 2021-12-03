4fb2a895bd98a9e1f092a64f8795e2c7867e62a2
Diego Martins (Amato) <devdiegomartins@gmail.com>
2021-12-03 15:17:05 -0300

    Finish Problem Set 1

--- Week 1/Problem Set 1/mario/mario.c
+++ Week 1/Problem Set 1/mario/mario.c
@@ -2,18 +2,34 @@
 #include <cs50.h>
 
 int h = 10;
+int i, j, k;
 
-int main (void)
+int main(void)
 {
+    //Name of de program
     printf("######## SUPER MARIO BROTHERS ########\n");
     printf("########  PYRAMID OF BLOCKS   ########\n");
 
+    //Prompt for pyramid height
     do
     {
         h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
 
-    
-    printf ("Height: %i", h);
+    //Loop for building the pyramid
+    for (i = 1; i <= h; i++)
+    {
+        for (k = h; k > i; k--)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
 }
